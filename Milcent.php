39fa95f45d27e74803e924f9608ae2a01b2e11d4
ilcent <musokimilcent@gmail.com>
2024-09-26 12:12:20 +0300

    Create Milcent.php

---
+++ Milcent.php
@@ -0,0 +1,10 @@
+<?php
+$name="milcent";
+$home_address="matugga";
+$date_of_birth="20/06/2000";
+$current_year=2024;
+$birth_year=2000;
+
+$age=$current_year - $birth_year;
+echo"Hello $name, welcome back home. Today you are making $age years";
+?>
